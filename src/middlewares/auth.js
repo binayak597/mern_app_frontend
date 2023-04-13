e961738c68019addc66df072b3dacefed16f76bf
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2023-04-13 13:59:08 +0530

    little changes have done

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,11 +1,11 @@
-import { Navigate} from 'react-router-dom';
 import { useAuthStore } from '../state/store';
+import Login from "../pages/Login";
 
 
 export const AuthorizeUser = ({children}) => {
     const token = localStorage.getItem("token");
     if(!token){
-        return <Navigate to={"/"} replace={true}></Navigate>
+        return <Login />;
     }
     return children;
 }
@@ -13,7 +13,7 @@ export const AuthorizeUser = ({children}) => {
 export const ProtectedRoute = ({children}) => {
     const {userName} = useAuthStore(state => state.auth);
     if(!userName){
-        return <Navigate to={"/"} replace={true}></Navigate>
+        return <Login />;
     }
     return children;
 }
